bf2d53bd3cca5980fd244328eb00ccced5ee8f57
Calleum Pecqueux <hi@calleum.au>
2023-06-29 16:01:46 +1000

    feat(edsl): add composition fns for templating

--- hello.hs
+++ hello.hs
@@ -1,13 +1,58 @@
+newtype Html = Html String
+newtype Element = Element String
+type Title = String
+
 main = do
-  putStrLn (makeHtml "My page!" "Hello, cletus!")
+  putStrLn (render myhtml)
 
-makeHtml :: String -> (String -> String)
-makeHtml title content = html_ ((head_ (title_ title)) <> (body_ content))
+myhtml :: Html
+myhtml =
+  html_
+    "My page!"
+    ( append_
+      (h1_ "Page Head1")
+      ( append_
+        (p_ "Hey cletus!")
+        (p_ "Hey zeus!")
+      )
+    )
 
+html_ :: Title -> Element -> Html
+html_ title content = Html
+    ( makeTag "html"
+      ( makeTag "head" (makeTag "title" title)
+      <> makeTag "body" (getElemString content)
+      )
+    )
 
+body_ :: String -> Element
+body_ = Element . makeTag "body"
+
+head_ :: String -> Element
+head_ = Element . makeTag "head"
+
+title_ :: String -> Element
+title_ = Element . makeTag "title"
+
+p_ :: String -> Element
+p_ = Element . makeTag "p"
+
+h1_ :: String ->  Element
+h1_ = Element . makeTag "h1"
+
+makeTag :: String -> String -> String
 makeTag tag content = "<" <> tag  <> ">" <> content <> "</" <> tag <> ">"
-html_ = makeTag "html"
-body_ = makeTag "body"
-head_ = makeTag "head"
-title_ = makeTag "title"
+
+render :: Html -> String
+render html = 
+    case html of
+        Html str -> str
+
+getElemString :: Element -> String
+getElemString el = 
+    case el of
+        Element str -> str
+
+append_ :: Element -> Element -> Element
+append_ a b = Element (getElemString a <> getElemString b)
 
